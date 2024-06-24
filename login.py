b5d96ec89da2e787087a6b54089b99fe93d340e4
QobilovOzodbek <ozodbekqobilovinfo@gmail.com>
2024-06-24 13:48:17 +0500

    edit code

--- login.py
+++ login.py
@@ -1,5 +1,17 @@
 from tkinter import *
+from tkinter import messagebox
 
+def login():
+    username = entry1.get()
+    password = entry2.get()
+
+    if (username=="" and password==""):
+        messagebox.showinfo("", "Blank not allowed")
+    elif (username=="admin" and password=="admin"):
+        messagebox.showinfo("", "Login success")
+    else:
+        messagebox.showinfo("", "Incorrect username and password")
+    
 root = Tk()
 root.title("Login system")
 root.geometry("400x250")
@@ -17,7 +29,5 @@ entry2 = Entry(root,bd=5)
 entry2.place(x=140,y=70)
 
 Button(root,text="Login",command=login,height=3,width=13,bd=6).place(x=100,y=120)
-
-    
-
+ 
 root.mainloop()
